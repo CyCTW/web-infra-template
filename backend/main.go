2e57108a5cad4f4c5f1572c71230a159192f1969
cyctw <yuaannn_192.cs06@g2.nctu.edu.tw>
2022-10-07 14:23:25 +0800

    add home path

--- backend/main.go
+++ backend/main.go
@@ -9,8 +9,14 @@ import (
 func hello(c *gin.Context) {
 	c.JSON(http.StatusOK, "hello world")
 }
+
+func home(c *gin.Context) {
+	c.JSON(http.StatusOK, "hello home")
+}
+
 func main() {
 	router := gin.Default()
+	router.GET("/", home)
 	router.GET("/api", hello)
 
 	router.Run()
